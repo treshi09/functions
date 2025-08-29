29ca392565fd9b98f0610ca513b71e96daa3ad1d
Your Name <you@example.com>
2025-08-29 17:25:46 +0100

    commit

---
+++ main.py
@@ -0,0 +1,6 @@
+#Write a program to create a function name well wishes that will give output hello, how are you!
+
+def well_wishes():
+    print("Hello, how are you!")
+
+well_wishes()    
---
+++ main1.py
@@ -0,0 +1,9 @@
+#Write a program to display the weather in autumn and spring :
+
+def weather():
+    print("The weather in autumn is", autumn)
+    print("The weather in spring is", spring)
+
+autumn = "pleasant"
+spring = "sunny"
+weather()
